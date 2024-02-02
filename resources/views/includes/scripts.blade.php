17e84cc65f62ac36ab959d2819ae6c0503641991
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-02-02 20:58:53 +0700

    cleave library add

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -5,6 +5,8 @@
 <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
 <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
 <script src="{{ asset('assets/js/app.js') }}"></script>
+<!-- cleave.js -->
+<script src="{{ asset('assets/libs/cleave.js/cleave.min.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 @livewireScripts
 
