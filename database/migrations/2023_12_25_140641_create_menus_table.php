8e189e13bde4a039bb5a170f4c31a7dca7aa4a07
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-02-27 17:22:44 +0700

    Add condition if menu is dropdown

--- database/migrations/2023_12_25_140641_create_menus_table.php
+++ database/migrations/2023_12_25_140641_create_menus_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('route_name')->nullable();
             $table->string('url')->nullable();
             $table->boolean('is_active')->default(true);
+            $table->boolean('is_dropdown')->default(true);
             $table->timestamps();
         });
     }
--- resources/views/components/layouts-app/menu.blade.php
+++ resources/views/components/layouts-app/menu.blade.php
@@ -1,22 +1,36 @@
 @if ($menu->children->isEmpty())
-    <li class="nav-item" @if($menu->parent) style="margin-left: 10px;" @endif>
+    <li class="nav-item" @if ($menu->parent) style="margin-left: 10px;" @endif>
         <a class="nav-link menu-link @if (request()->is($menu->url . '*')) active @endif" href="{{ $menu->route_name && Route::has($menu->route_name) ? route($menu->route_name) : '/' }}">
             <i class="{{ $menu->icon }} me-3"></i><span>{{ $menu->label_name }}</span>
         </a>
     </li>
 @else
-    <li class="nav-item">
-        <a class="nav-link menu-link @if (request()->is($menu->url . '*')) active @endif" href="#menu-{{ $menu->id }}" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="menu-{{ $menu->id }}">
-            <i class="{{ $menu->icon }} me-1"></i><span>{{ $menu->label_name }}</span>
-        </a>
-        <div class="collapse menu-dropdown @if (request()->is($menu->url . '*')) show @endif" id="menu-{{ $menu->id }}">
-            <ul class="nav flex-column">
-                @foreach ($menu->children as $child)
-                    @if ($child->is_active == 1)
-                        <x-layouts-app.menu :menu="$child" />
-                    @endif
-                @endforeach
-            </ul>
-        </div>
-    </li>
+    @if ($menu->is_dropdown)
+        <li class="nav-item">
+            <a class="nav-link menu-link @if (request()->is($menu->url . '*')) active @endif" href="#menu-{{ $menu->id }}" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="menu-{{ $menu->id }}">
+                <i class="{{ $menu->icon }} me-1"></i><span>{{ $menu->label_name }}</span>
+            </a>
+            <div class="collapse menu-dropdown @if (request()->is($menu->url . '*')) show @endif" id="menu-{{ $menu->id }}">
+                <ul class="nav flex-column">
+                    @foreach ($menu->children as $child)
+                        @if ($child->is_active == 1)
+                            <x-layouts-app.menu :menu="$child" />
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
+        </li>
+    @else
+        <li class="menu-title"> 
+            <span>
+                <i class="{{ $menu->icon }} me-1"></i>
+                {{ $menu->label_name }}
+            </span>
+        </li>
+        @foreach ($menu->children as $child)
+            @if ($child->is_active == 1)
+                <x-layouts-app.menu :menu="$child" />
+            @endif
+        @endforeach
+    @endif
 @endif
