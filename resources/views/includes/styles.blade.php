e29df1774cfb5fcb4d4d63935b05ef2d790e2e54
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-03-09 12:22:21 +0700

    Style: change font and icon size menu

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -11,6 +11,17 @@
 <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- custom Css-->
 <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+<style>
+    .navbar-menu .navbar-nav .nav-link i {
+        display: inline-block;
+        min-width: 1.2rem;
+        font-size: 15px;
+    }
+
+    .navbar-menu .navbar-nav .nav-link {
+        font-size: 16px;
+    }
+</style>
 @stack('styles')
 <!-- Font Awesome -->
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/icons/fontawesome.css') }}" />
