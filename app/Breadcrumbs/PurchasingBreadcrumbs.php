d5a5f07ec5d6d4c47ae76cb9681980708da9cff3
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-03-17 15:48:23 +0700

    Feat (purchasing): new breadcrumbs

--- app/Breadcrumbs/PurchasingBreadcrumbs.php
+++ app/Breadcrumbs/PurchasingBreadcrumbs.php
@@ -12,6 +12,12 @@ Breadcrumbs::for('purchasing.invoice.index', function($trail) {
     $trail->push('Invoice List');
 });
 
+Breadcrumbs::for('purchasing.invoice.create', function($trail) {
+    $trail->parent('purchasing');
+    $trail->push('Product', route('purchasing.invoice.create'));
+    $trail->push('Add Purchasing Invoice');
+});
+
 Breadcrumbs::for('purchasing.retur.index', function($trail) {
     $trail->parent('purchasing');
     $trail->push('Retur', route('purchasing.retur.index'));
