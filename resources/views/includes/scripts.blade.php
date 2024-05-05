210e3588091c59678743c70c24c1b12c1abecdc2
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-05-05 11:09:42 +0700

    Fix: jquery assets tag scripts

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -4,10 +4,11 @@
 <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}" data-navigate-once></script>
 <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}" data-navigate-once></script>
 <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}" data-navigate-once></script>
-<script src="{{ asset('assets/js/app.js') }}" data-navigate-once></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
 <!-- cleave.js -->
 <script src="{{ asset('assets/libs/cleave.js/cleave.min.js') }}" data-navigate-once></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" data-navigate-once></script>
+{{-- JQuery --}}
+<script src="{{ asset('assets/libs/jquery/jquery-3.6.0.min.js') }}" data-navigate-once></script>
 @livewireScripts
 
 @stack('scripts')
