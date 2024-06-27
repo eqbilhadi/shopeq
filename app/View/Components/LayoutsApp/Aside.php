eb1b09fdf941afd45d6bb4cada76b8dec237181b
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-06-27 12:03:00 +0700

    Refactore: add eager loading on load menu navigation

--- app/View/Components/LayoutsApp/Aside.php
+++ app/View/Components/LayoutsApp/Aside.php
@@ -69,7 +69,7 @@ class Aside extends Component
                 'is_active' => '1',
             ])
             ->with([
-                'children' => function ($query) use ($user) {
+                'children.parent' => function ($query) use ($user) {
                     $query->whereHas('roles', fn ($query) => $query->whereIn('role_id', $user->roles->pluck('id')));
                 }
             ])
